80a275cbbfe73c067e815de3c7fa68b822c5d7a0
William Long <willhlong@gmail.com>
2023-09-18 10:16:22 -0400

    Added basic body content to home module

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,23 @@ import '../style.css';
 import header from './header.js';
 
 function home() {
-    return header();
+    header();
+    
+    const content = document.getElementById('content');
+    const homeScreen = document.createElement('div');
+    const welcomeMessage = document.createElement('h1');
+    welcomeMessage.textContent = "Welcome to Mi Arepita!";
+    homeScreen.appendChild(welcomeMessage);
+
+    const descriptionOne = document.createElement('h3');
+    descriptionOne.textContent = "The best Colombian food this side of Colombia";
+    homeScreen.appendChild(descriptionOne);
+
+    const menu = document.createElement('button');
+    menu.textContent = 'Menu';
+    homeScreen.appendChild(menu);
+    content.appendChild(homeScreen);
+    return content;
 }
 
 export default home;
