1a994683d58830f1a9ae98bd70a7d5d70dfb69d5
William Long <willhlong@gmail.com>
2023-09-17 21:39:10 -0400

    Basic home page added with non-functioning nav-bar

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import home from './pages/home.js';
 import about from './pages/about.js';
 import menu from './pages/menu.js';
 
-home();
-about();
-menu();
+document.body.appendChild(home());
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,40 @@
+import menu from "./menu";
+import '../style.css';
+
 function home() {
-    console.log('home tab');
+    const content = document.createElement('div');
+    content.id = 'content';
+
+    const header = document.createElement('div');
+    header.classList += 'header';
+
+    const logo = document.createElement('div');
+    logo.textContent = 'Mi Arepita';
+
+    const tabs = document.createElement('div');
+    tabs.classList += 'tabs-wrapper';
+
+    const homeTab = document.createElement('div');
+    homeTab.classList += 'tabs';
+    homeTab.textContent = 'Home';
+    tabs.appendChild(homeTab);
+
+    const menuTab = document.createElement('div');
+    menuTab.classList += 'tabs';
+    menuTab.textContent = 'Menu';
+    tabs.appendChild(menuTab);
+
+    const aboutTab = document.createElement('div');
+    aboutTab.classList += 'tabs';
+    aboutTab.textContent = 'About Us';
+    tabs.appendChild(aboutTab);
+
+    header.appendChild(logo);
+    header.appendChild(tabs);
+    content.appendChild(header);
+
+    return content;
+
 }
 
 export default home;
